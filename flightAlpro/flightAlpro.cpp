1f08b1f6c6273c4879454667dcd76451142ad601
Adyatma <69962558+lafuanh@users.noreply.github.com>
2023-12-03 18:27:00 +0700

    Update flightAlpro.cpp

--- flightAlpro/flightAlpro.cpp
+++ flightAlpro/flightAlpro.cpp
@@ -10,6 +10,9 @@ using namespace std;
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <iomanip>
+
 
 // DATA MASKAPAI
 std::string dataMaskapai[][2] = {
@@ -33,7 +36,8 @@ int jumlahMaskapai = sizeof(dataMaskapai) / sizeof(dataMaskapai[0]);
 struct Pesawat {
     int idPesawat;
     string idMaskapai;
-    int tipePesawat; //1 large: 240, 2 medium: 140,3 small: 80 -- 25% are bussnies 50% are economy 10% are first class, 15% are quite zone
+    int tipePesawat; //1 large: 240, 2 medium: 140,3 small: 80 --
+    //25% are bussnies 50% are economy 10% are first class, 15% are quite zone
 };
 
 struct kursi
@@ -56,14 +60,6 @@ std::string Bandara[][2] = {
 };
 int JumlahBandara = sizeof(Bandara) / sizeof(Bandara[0]);
 
-struct Penerbangan
-{
-    int nomorPesawat;
-    int tanggalPenerbangan;
-    string status;
-    string id_BandaraTujuan;
-    string id_BandaraSekarang;
-};
 
 struct dataDiri
 {
@@ -71,32 +67,12 @@ struct dataDiri
     int usia;
     string nomorhp;
     void display() {
-
-
+        cout<< "Nama :" << nama<< endl;
+        cout<< "usia :" << usia<< endl;
+        cout<< "usia :" << nomorhp<< endl;
     }
 };
 
-struct Tiket
-{
-    int idTicket;
-    int idPesawat;
-    int tanggalBerangkat; // time clock __ day / month / year
-    string id_bandaraSaatIni;
-    string id_tujuanBerangkat;
-    int gate; //random pintu masuk
-    //data diri
-
-    void display(){
-    //airticket
-    //passanger
-    // 
-    // idCurrent airport to next airport
-    // 
-    //date depature
-    //
-    
-    }
-};
 
 //------------------------------//
 std::vector<Pesawat> dataPesawat;
@@ -121,25 +97,223 @@ void generatePesawat() {
     }
 }
 
-void startAplication() {
-    generatePesawat();
-    //generate penerbangan -- set every day had to be 3 plane each airport to random airport
+//-----//
+struct Penerbangan
+{
+    int nomorPesawat;
+    int tanggalPenerbangan;
+    int jamKeberangkatan;
+
+    string status;
+    string id_BandaraTujuan;
+    string id_BandaraSekarang;
+    string id_gate;
+
+};
+
+std::vector<Penerbangan> dataPenerbangan;
+
+Penerbangan createPenerbangan(int nomorPesawat,int date,int depature, string codeTujuan, string codeBandaraAwal, string gate) {
+    Penerbangan penerbangan_new;
+    penerbangan_new.nomorPesawat= nomorPesawat;
+    penerbangan_new.tanggalPenerbangan = date;
+    penerbangan_new.jamKeberangkatan = depature;
+    penerbangan_new.status = "null";
+    penerbangan_new.id_BandaraTujuan = codeTujuan;
+    penerbangan_new.id_BandaraSekarang = codeBandaraAwal;
+    penerbangan_new.id_gate = gate;
+    return penerbangan_new;
+}
+
+void generatePenerbangan(){
+    int countPlane= 0;
+    std::srand(static_cast<unsigned int>(std::time(0)));
+    string numberGate;
+    for(int i = 0; i < JumlahBandara;i++){
+
+        for(int j = 0; j < 7;j++){
+            
+            for(int k =0; k < 3;k++){
+                int randomNumberBandara = std::rand() % 10;
+                int depature = 0; //in minutes
+                
+                if(randomNumberBandara == i && randomNumberBandara != JumlahBandara - 1){
+                    randomNumberBandara++;
+                }else if(randomNumberBandara == i && randomNumberBandara == JumlahBandara - 1){
+                    randomNumberBandara--;
+                }
+                
+                switch (k) {
+                    case 0:
+                        depature = std::rand() % 481;
+                        numberGate = "A-1";
+                        break;
+                    case 1:
+                        depature = std::rand() % 481;
+                        numberGate = "A-2";
+
+                        break;
+                    case 2:
+                        depature = std::rand() % 481 + 960;
+                        numberGate = "B-1";
+
+                        break;
+                    default:
+                        break;
+                }
+            
+                
+                
+                dataPenerbangan.push_back(createPenerbangan(dataPesawat[countPlane].idPesawat, j+ 1,depature, Bandara[randomNumberBandara][1],Bandara[i][1],numberGate));
+
+                countPlane++;
+
+            }
+        }
+    }
+    
 }
 
 
+void startAplication() {
+    generatePesawat();
+    generatePenerbangan();
+    //generate penerbangan -- set every day had to be 3 plane each airport to random airport
+    
+}
+
+//---------- FUNGSI Flight board ---------//
+int showActualTime(){
+    auto currentTime = std::chrono::system_clock::now();
+    auto timeNow = std::chrono::system_clock::to_time_t(currentTime);
+    auto localTime = std::localtime(&timeNow);
+
+    int jam = localTime->tm_hour;
+    int menit = localTime->tm_min;
+    
+    return jam * 60 + menit;
+    
+}
+
+string convertJam(int minutes) {
+    int hours = minutes / 60;
+    int remainingMinutes = minutes % 60;
+
+    std::string hoursStr = std::to_string(hours);
+    std::string minutesStr = std::to_string(remainingMinutes);
+
+    if (hours < 10) {
+        hoursStr = "0" + hoursStr;
+    }
+    if (remainingMinutes < 10) {
+        minutesStr = "0" + minutesStr;
+    }
+    return hoursStr + ":" + minutesStr;
+}
+
+string convertKodePesawat(int nomorPesawat) {
+    string kodeMaskapai;
+    string kodeAkhir = std::to_string(nomorPesawat);
+    for (const auto& pesawat : dataPesawat) {
+        if (pesawat.idPesawat == nomorPesawat) {
+            kodeMaskapai = pesawat.idMaskapai;
+        }
+    }
+    return kodeMaskapai + " " + kodeAkhir;
+}
+
+void updateStatus(int currentDate){
+    int jam = showActualTime();
+    for ( auto& penerbangan : dataPenerbangan) {
+        if(penerbangan.tanggalPenerbangan == currentDate){
+            if(penerbangan.jamKeberangkatan < jam ){
+                penerbangan.status = "gate closed";
+            }else if(penerbangan.tanggalPenerbangan > jam && penerbangan.tanggalPenerbangan <= jam - 90){
+                penerbangan.status = "gate open";
+            }else{
+                penerbangan.status = "Boarding";
+
+            }
+        }
+    }
+}
 
 
+void DisplayBoard(string airportCode, int date){
+    int jam = showActualTime() ;
+    
+    cout<< "\njam: "<< convertJam(jam) << "\ntanggal: "<< date<< " Desember 2023\n" <<endl;
+    cout << std::setw(15) << std::left << "Kode Pesawat"
+               << std::setw(20) << "Depature"
+               << std::setw(15) << "Remarks"
+               <<  std::setw(15) << "Gate"
+              <<std::setw(15) << "Tujuan"<< endl;
+    
+     for (const auto& penerbangan : dataPenerbangan) {
+         if (penerbangan.id_BandaraSekarang == airportCode && penerbangan.tanggalPenerbangan == date) {
+             
+             std::cout << std::setw(15) << convertKodePesawat(penerbangan.nomorPesawat)
+                       << std::setw(20) << convertJam(penerbangan.jamKeberangkatan)
+                        << std::setw(15) << penerbangan.status
+                       << std::setw(15) <<  penerbangan.id_gate
+                      << std::setw(15) << penerbangan.id_BandaraTujuan<< "\n"<<std::endl;
+         }
+     }
+}
+//---------------------
+struct Tiket
+{
+    int idTicket;
+    int idPesawat;
+    int tanggalBerangkat; // time clock __ day / month / year
+    string id_bandaraSaatIni;
+    string id_tujuanBerangkat;
+    int jamDepature;
 
+    string gate; //random pintu masuk
+    string namaPenumpang;
+    //data diri
 
+    void display(){
+        cout<< "[------ AIR TICKET ------]"<<endl;
+        cout<< "asal bandara: " << id_bandaraSaatIni << endl;
+        cout<< "     VVV"<<endl;
+        cout<< "tujuan: " << id_tujuanBerangkat<<endl;
+        
+        cout<< "\npesawat: " << convertKodePesawat(idPesawat)<<endl;
 
+        
+        cout<< "\nPenumpang: "<< namaPenumpang<<endl;
+        cout<< "gate: " << gate<<endl;
+        cout << "Depature: "<< convertJam(jamDepature)<<" " <<tanggalBerangkat<<endl;
+        
+        cout << "\nticket: "<<idTicket<<endl;
+        cout<< "[-----------------------]"<<endl;
 
+    }
+};
 
+std::vector<Tiket> dataTicket;
 
-
+Tiket createTiket(int nTiket, int nPesawat, int tanggalPesawat, int timeDepature, string idBandaraNow,string idTujuan,string idGate, string nama) {
+    Tiket Ticket_new;
+    Ticket_new.idTicket = nTiket;
+    Ticket_new.idPesawat = nPesawat;
+    Ticket_new.tanggalBerangkat = tanggalPesawat;
+    Ticket_new.jamDepature = timeDepature;
+    Ticket_new.id_bandaraSaatIni = idBandaraNow;
+    Ticket_new.id_tujuanBerangkat = idTujuan;
+    Ticket_new.gate = idGate;
+    Ticket_new.namaPenumpang = nama;
+    return Ticket_new;
+}
 
 
 int main() {
+    int hari_now = 3;
     startAplication();
+    updateStatus(hari_now);
+    DisplayBoard("SOC", 3);
 
     string menu_app[6] = {
         "1. Admin",
@@ -148,49 +322,10 @@ int main() {
         "4. Exit"
     };
 
-    string makanan[30], minuman[30];
-    float harga_makanan[30], harga_minuman[30];
-    int pesanan_makanan[30], pesanan_minuman[30];
-    int n_makanan = 3;
-    int n_minuman = 3;
 
-
-    int n_pesan_makanan = 0;
-    int n_pesan_minuman = 0;
-
-
-    //makanan
-    makanan[0] = "Sop Buntut";
-    makanan[1] = "Nasi Goreng";
-    makanan[2] = "Bebek Bakar";
-
-    //minuman
-    minuman[0] = "Stroawberry Squash";
-    minuman[1] = "Orange Juice";
-    minuman[2] = "Lemon Tea";
-
-    //harga makanan
-    harga_makanan[0] = 45000;
-    harga_makanan[1] = 20000;
-    harga_makanan[2] = 40000;
-
-    //harga minuman
-    harga_minuman[0] = 7000;
-    harga_minuman[1] = 15000;
-    harga_minuman[2] = 7000;
-
-   
  
-    bool isDone = true;
-    int harga = 0;
-    std::string tambahanMenu = "";
-
-
     bool is_lanjut = true;
     int i, pilih;
-    float total_pesanan;
-    int inputcode;
-
 
     while (is_lanjut) {
         for (i = 0;i < 6;i++) {
@@ -198,140 +333,17 @@ int main() {
         }
         cout << "Pilih Menu: ";
         cin >> pilih;
-
-
         switch (pilih) {
         case 1:
-            cout << "-----------------------\n";
-            for (i = 0;i < n_makanan;i++) {
-                cout << makanan[i] << " (kode: " << i << ")\n";
-            }
-            cout << "-----------------------\n";
-            cout << "Input kode: ";
-            cin >> pesanan_makanan[n_pesan_makanan];
-            n_pesan_makanan++;
             break;
         case 2:
             cout << "-----------------------\n";
-            for (i = 0;i < n_minuman;i++) {
-                cout << minuman[i] << " (kode: " << i << ")\n";
-            }
-            cout << "-----------------------\n";
-            cout << "Input kode: ";
-            cin >> pesanan_minuman[n_pesan_minuman];
-            n_pesan_minuman++;
+         
             break;
         case 3:
-            total_pesanan = 0;
             cout << "----------makanan-------------\n";
-            for (i = 0;i < n_pesan_makanan;i++) {
-                cout << makanan[pesanan_makanan[i]];
-                cout << " (harga: ";
-                cout << harga_makanan[pesanan_makanan[i]];
-                cout << ")\n";
-                total_pesanan += harga_makanan[pesanan_makanan[i]];
-            }
-            cout << "----------minuman-------------\n";
-            for (i = 0;i < n_pesan_minuman;i++) {
-                cout << minuman[pesanan_minuman[i]];
-                cout << " (harga: ";
-                cout << harga_minuman[pesanan_minuman[i]];
-                cout << ")\n";
-                total_pesanan += harga_minuman[pesanan_minuman[i]];
-            }
-            cout << "-----------total--------------\n";
-            cout << "Total: " << total_pesanan << endl;
-            cout << "------------------------------\n";
             break;
         case 4:
-            isDone = true;
-            //  addMenu();
-            while (isDone) {
-                harga = 0;
-                tambahanMenu = "";
-
-                int menu;
-                cout << "pilih menu: \n 1. Tambah Menu Makanan \n 2. tambah menu minuman \n 3. Quit \n" << endl;
-                cout << "Pilih nomor Menu: ";
-
-                cin >> menu;
-                if (menu == 1) {
-                    std::cout << "nama menu baru: ";
-                    cin.ignore();
-                    std::getline(std::cin, tambahanMenu);
-                    std::cout << "harga menu baru: ";
-
-                    std::cin >> harga;
-                    cout << harga << endl;
-
-                    makanan[n_makanan] = tambahanMenu;
-                    harga_makanan[n_makanan] = harga;
-                    n_makanan++;
-                }
-                else if (menu == 2) {
-                    std::cout << "nama menu baru: ";
-                    cin.ignore();
-                    std::getline(std::cin, tambahanMenu);
-                    std::cout << "harga menu baru: ";
-                    std::cin >> harga;
-
-                    minuman[n_minuman] = tambahanMenu;
-                    harga_minuman[n_minuman] = harga;
-                    n_minuman++;
-                }
-                else if (menu == 3) {
-                    isDone = false;
-                }
-            }
-            break;
-        case 5:
-            isDone = true;
-            //  addMenu();
-            while (isDone) {
-                tambahanMenu = "";
-
-
-                int menu;
-                cout << "pilih menu: \n 1. ganti Menu Makanan \n 2. ganti menu minuman \n 3. Quit \n" << endl;
-                cout << "Pilih nomor Menu: ";
-                cin >> menu;
-                if (menu == 1) {
-
-                    for (i = 0;i < n_makanan;i++) {
-                        cout << makanan[i] << " (kode: " << i << ")\n";
-                    }
-
-                    cout << "-----------------------\n";
-                    cout << "Input kode makanan yang akan diedit: ";
-                    cin >> inputcode;
-
-                    std::cout << "update nama menu : ";
-                    cin.ignore();
-                    std::getline(std::cin, tambahanMenu);
-
-                    makanan[inputcode] = tambahanMenu;
-                }
-                else if (menu == 2) {
-                    for (i = 0;i < n_minuman;i++) {
-                        cout << minuman[i] << " (kode: " << i << ")\n";
-                    }
-
-                    cout << "-----------------------\n";
-                    cout << "Input kode minuman yang akan diedit: ";
-                    cin >> inputcode;
-
-                    std::cout << "update nama menu : ";
-                    cin.ignore();
-                    std::getline(std::cin, tambahanMenu);
-                    minuman[inputcode] = tambahanMenu;
-
-                }
-                else if (menu == 3) {
-                    isDone = false;
-                }
-            }
-            break;
-        case 6:
             is_lanjut = false;
             break;
 
